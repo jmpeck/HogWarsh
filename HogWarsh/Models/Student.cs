9f1c32728e2f6f8afc5e8a9dcd79f88cc2d3b8b1
John <john_m_peck@yahoo.com>
2017-06-05 19:20:35 -0400

    Initial Commit. Created basic html form

---
+++ HogWarsh/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HogWarsh.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+
+        public Species Species { get; set; }
+
+        public House House { get; set; }
+    }
+
+    public enum Species
+    {
+        Feline,
+        Canine,
+        Fish
+    }
+}
